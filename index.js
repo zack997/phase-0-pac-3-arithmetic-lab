14d51d9ca4ec552e885d27d58b431df97e1818b2
zacknjenga <izackkiarie@gmail.com>
2024-04-04 21:48:15 +0300

    adding arithmetic

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function divide(a, b) {
+    return a / b;
+  }
+
+  number = 5;
+
+  function addn() {
+    return (number += n);
+  }
+
+  function subtractn() {
+    return (number -= n);
+  }
+
+  function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+
+function makeInt(n) {
+    return parseInt(n);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
